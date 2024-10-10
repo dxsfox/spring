10ce9adb32870e208fe09d1a4efd6b3d2de00c8d
AP23040 ダイ <134293749+dxsfox@users.noreply.github.com>
2024-10-10 10:22:53 +0900

    props and bolean

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './src/components/hello';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang={false}>World</Hello>
       <Text>DAI</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,9 +16,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f458',
-    
+    backgroundColor: '#a4545',
+  
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
---
+++ src/components/hello.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+
+interface Props{
+    children:string
+    bang:boolean
+}
+
+const hello=(props:Props):JSX.Element=>{
+    // const children =props.children
+    const{children,bang}=props
+    return(
+        <View>
+        <Text style={styles.text}>
+        Hello{children}{bang ?'!':''}
+        </Text>
+    </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ff45',
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+    text:{
+        color:'#a4565',
+       backgroundColor:'orange',
+        fontSize:150,
+        fontWeight:'bold',
+        padding:16
+    }
+  });
+  
+export default hello
